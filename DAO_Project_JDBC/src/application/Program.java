caf70459e47429f1d7ac48224135a231c854ee73
BrenoRev <silvabreno462@gmail.com>
2021-05-27 07:25:10 -0300

    Seller Entity Class

--- DAO_Project_JDBC/src/application/Program.java
+++ DAO_Project_JDBC/src/application/Program.java
@@ -1,9 +1,8 @@
 package application;
 
-import db.DB;
-import java.sql.Connection;
-import java.sql.Statement;
+import java.util.Date;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -12,6 +11,8 @@ public class Program {
         Department obj = new Department(1, "books");
         System.out.println(obj);
         
+        Seller seller = new Seller(21,"Bob","bob@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
     }
 
     
--- DAO_Project_JDBC/src/model/entities/Department.java
+++ DAO_Project_JDBC/src/model/entities/Department.java
@@ -60,7 +60,7 @@ public class Department implements Serializable{
 
     @Override
     public String toString() {
-        return "Department [ ID: " + id + " | Name: " + name + " ]";
+        return "[ ID: " + id + " | Name: " + name + " ";
     }
     
     
